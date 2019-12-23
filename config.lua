91a3db9076b1217c921afba37cca2b7a83bc5e95
Rodrigues23413 <59182025+Rodrigues23413@users.noreply.github.com>
2019-12-23 21:55:36 +0000

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,25 @@
+Config              = {}
+Config.MarkerType   = 1
+Config.DrawDistance = 100.0
+Config.ZoneSize     = {x = 5.0, y = 5.0, z = 3.0}
+Config.MarkerColor  = {r = 100, g = 204, b = 100}
+Config.RequiredCopsCoke = 4
+Config.RequiredCopsMeth = 4
+Config.RequiredCopsWeed = 4
+Config.RequiredCopsOpium = 4
+Config.Locale = 'en'
+
+Config.Zones = {
+	CokeFarm = 		 {x=-996.96,  y=4853.86, z=274.61},
+	CokeTreatment =  {x=-2431.67, y=4961.16, z=42.35},
+	CokeResell = 	 {x=435.71, y=-2064.67,  z=28.09},
+	MethFarm = 		 {x=0, 	 y=0,  z=0},
+	MethTreatment =  {x=0, y=0,  z=0},
+	MethResell = 	 {x=-0, y=-0, z=0},
+	WeedFarm = 		 {x=-1753.69, y=2173.55,  z=115.48},
+	WeedTreatment =  {x=-1909.1,  y=1388.96,  z=218.95},
+	WeedResell = 	 {x=-97.77, y=-1638.77 , z=35.48},
+	OpiumFarm = 	 {x=0,	 y=0,  z=0},
+	OpiumTreatment = {x=0,y=0, z=0},
+	OpiumResell = 	 {x=0,y=0,z=0}
+}
